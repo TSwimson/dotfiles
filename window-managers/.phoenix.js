c00cde4b49fc1ee71bf0970a33ac6bcf0579f02a
Tanner Swenson <tanner@youplus.co>
2014-08-17 21:00:53 -0700

    Updated phoenix

--- window-managers/.phoenix.js
+++ window-managers/.phoenix.js
@@ -1,7 +1,25 @@
 lookOfDisapproval = "ಠ_ಠ";
 rageOfDongers = "";
 whyLook = "ლ(ಠ_ಠლ)";
+var bound_keys = {};
+// bound_keys.b = {}
+// bound_keys.b.create_custom = null
 // Start/select apps
+//bound_keys = { i: { custom_binding: hotkey, create_custom: hotkey, bound: bool}}
+
+App.create_app_binding = function(key) {
+
+    api.alert(key)
+    bound_keys[key].create_custom.disable();
+    app_title = Window.focusedWindow().app().title();
+    //bound_keys[key].custom_binding =
+    api.alert('binding');
+    api.bind(key, hyper, function() {
+        App.focusOrStart(app_title);
+    });
+
+};
+
 App.allWithTitle = function(title) {
     return _(this.runningApps()).filter(function(app) {
         if (app.title() === title) {
@@ -23,10 +41,11 @@ App.focusOrStart = function(title) {
         })
         .flatten()
         .value();
-
-    activeWindows = _(windows).reject(function(win) {
-        return win.isWindowMinimized();
-    });
+    activeWindows = windows; //TODO
+    // this code disables switching to apps which have all apps minimized
+    // activeWindows = _(windows).reject(function(win) { 
+    //     return win.isWindowMinimized();
+    // });
     if (_.isEmpty(activeWindows)) {
         api.alert(whyLook + " All windows minimized for " + title);
         return;
@@ -77,8 +96,6 @@ function toBottom(fillCols, maxCols) {
     var win = Window.focusedWindow();
     var frame = win.frame();
     var screenFrame = win.screen().frameWithoutDockOrMenu();
-    api.alert('fx: ' + frame.y);
-    api.alert('sx: ' + screenFrame.y);
     frame.y = screenFrame.y + (screenFrame.height * (fillCols / maxCols));
     frame.x = screenFrame.x;
     frame.height = screenFrame.height * (fillCols / maxCols);
@@ -99,22 +116,22 @@ var finder = 'Finder'
 
 // window size adjust keys
 api.bind('left', hyper, function() {
-    toLeft(1, 2);
+    toLeft(6, 10);
 });
 api.bind('right', hyper, function() {
-    toRight(1, 2);
+    toRight(6, 10);
 });
 api.bind('up', hyper, function() {
-    toTop(1, 2);
+    toTop(6, 10);
 });
 api.bind('down', hyper, function() {
-    toBottom(1, 2);
+    toBottom(4, 10);
 });
 api.bind('return', hyper, fullScreen);
 
 
 // Some kind of hinting
-api.bind('space', hyper, function() {});
+api.bind('space ', hyper, function() {});
 
 // App hotkeys
 api.bind('c', hyper, function() {
@@ -141,3 +158,37 @@ api.bind('x', hyper, function() {
 api.bind('s', hyper, function() {
     App.focusOrStart('System Preferences')
 });
+api.bind('r', hyper, function() {
+    App.focusOrStart('Preview')
+});
+api.bind('z', hyper, function() {
+    App.focusOrStart('DigitalColor Meter')
+});
+
+// bound_keys.b.create_custom = api.bind('b', hyper, function() {
+//     App.create_app_binding('b')
+// });
+
+bindable_keys = 'plokmijnuhb'.split('');
+bound_keys = {};
+//bound_keys = { i: { custom_binding: hotkey, create_custom: hotkey, bound: bool}}
+
+for (var i = 0; i < bindable_keys.length; i++) {
+    var key = bindable_keys[i];
+    bound_keys[key] = {};
+    bound_keys[key].bound = false;
+    api.alert(key);
+    bound_keys[key].create_custom = api.bind(key, hyper, function() {
+
+        App.create_app_binding(key);
+    });
+}
+
+
+
+
+
+
+
+
+///asdasdpl
